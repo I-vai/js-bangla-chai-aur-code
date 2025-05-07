d221ec7603414b8b22fd5fd81172dd2dbe5e89e5
Imran Vai <imranhossain002031@gmail.com>
2025-05-07 14:54:05 -0800

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+let accountState;
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all JS code as newr version
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("hitesh"); //it's necessary that ;- is a must
